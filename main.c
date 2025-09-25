ce6dfffe7dde7a37af314f0211461f33fa5453cf
dla1934-byte <dla1934@sookmyung.ac.kr>
2025-09-25 15:38:14 +0900

    2

--- main.c
+++ main.c
@@ -3,16 +3,22 @@
 
 int main(int argc, char *argv[])
 {
-  int x, y, z, m;
-  int a, b, c;
-  x=2;
-  z=1;
-  a=3;
-  b=4;
-  c=5;
-  y=a*x*x+b*x+c;
-  m=(x+y+z)/3;
-  printf("y=%d, m=%d\n", y, m);
+    int x,y;
+    
+    printf("input two integers :");
+    scanf( " %i %i", &x, &y);
+    
+    
+    
+   
+    
+    printf("+ result is : %i\n", x+y);
+    printf("- result is : %i\n", x-y);
+    printf("* result is : %i\n", x*y);
+    printf("/ result is : %i\n", x/y);
+    printf("%% result is : %i\n", x%y);
+
+
   
   
   system("PAUSE");	

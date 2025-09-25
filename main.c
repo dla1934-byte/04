1f190056dcf7057847a044b2949751dfb03d5347
dla1934-byte <dla1934@sookmyung.ac.kr>
2025-09-25 16:22:57 +0900

    6

--- main.c
+++ main.c
@@ -3,20 +3,16 @@
 
 int main(int argc, char *argv[])
 {
-    unsigned int x;
-    int b;
+    int sec;
     
-    printf("input a number:");
-    scanf("%ui", &x);
+    printf("Input seconds: ");
+    scanf("%i", &sec);
     
-    for (b=0; x!=0; x >>=1)
-    {
-        if (x & 1)
-        {
-        b++;
-        }     
-    }
-    printf("%i", b);
+    printf("The time is %i:%i:%i\n",
+                                     sec/3600,
+                                     (sec%3600)/60,
+                                     sec%60
+                                     );
    
   
   system("PAUSE");	
